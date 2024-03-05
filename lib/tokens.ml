cb31da3bf3695eecab27a71de7d0566569ab248e
mynameismon <42601474+mynameismon@users.noreply.github.com>
2024-03-05 19:33:38 +0530

    feat: add tokens to the parser

---
+++ lib/tokens.ml
@@ -0,0 +1,90 @@
+(* Is it possible to have Bracket = LeftParen | RightParen,
+   Operator = Plus | Minus | Star, etc.
+   and then make TokenType = Bracket | Operator | ...?*)
+
+type token_type =
+  | LeftParen
+  | RightParen
+  | LeftBrace
+  | RightBrace
+  | Comma
+  | Dot
+  | Plus
+  | Minus
+  | Star
+  | Slash
+  | Semicolon
+  | Bang
+  | BangEqual
+  | Equal
+  | EqualEqual
+  | Greater
+  | GreaterEqual
+  | Less
+  | LessEqual
+  | And
+  | Class
+  | Else
+  | False
+  | Fun
+  | For
+  | If
+  | Nil
+  | Or
+  | Print
+  | Return
+  | Super
+  | This
+  | True
+  | Var
+  | While
+  | String of string
+  | Number of float
+  | Identifier of string
+
+type token = {
+    kind: token_type;
+    line_number: int;
+    column: int
+  }
+
+let token_to_string token =
+  match token with 
+  | LeftParen -> "("
+  | RightParen -> ")"
+  | LeftBrace -> "{"
+  | RightBrace -> "}"
+  | Comma -> ","
+  | Dot -> "."
+  | Plus -> "+"
+  | Minus -> "-"
+  | Star -> "*"
+  | Slash -> "/"
+  | Semicolon -> ";"
+  | Bang -> "!"
+  | BangEqual -> "!="
+  | Equal -> "="
+  | EqualEqual -> "=="
+  | Greater -> ">"
+  | GreaterEqual -> ">="
+  | Less -> "<"
+  | LessEqual -> "<="
+  | And -> "and"
+  | Class -> "class"
+  | Else -> "else"
+  | False -> "false"
+  | Fun -> "fun"
+  | For -> "for"
+  | If -> "if"
+  | Nil -> "nil"
+  | Or -> "or"
+  | Print -> "print"
+  | Return -> "return"
+  | Super -> "super"
+  | This -> "this"
+  | True -> "true"
+  | Var -> "var"
+  | While -> "while"
+  | String str -> Printf.sprintf "\"%s\"" str
+  | Number num -> Float.to_string num
+  | Identifier idf -> idf;
