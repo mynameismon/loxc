1b834554eee9685a80a8fdc0f3d2760bc1849050
mynameismon <42601474+mynameismon@users.noreply.github.com>
2024-07-05 23:32:06 +0530

    feat: introduce tests into the interpreter

--- lib/ast.ml
+++ lib/ast.ml
@@ -29,3 +29,18 @@ let rec print_ast_node token =
   | Error str -> print_error str
 
 let print_ast tokens = print_ast_node tokens
+
+(* Literal tests *)
+let%test "print nil" = (String.equal "Nil" (print_ast_node (Literal(Nil))))
+let%test "print true" = (String.equal "true" (print_ast_node (Literal(Bool true))))
+let%test "print false" = (String.equal "false" (print_ast_node (Literal(Bool false))))
+let%test "print number" = (String.equal "1.000000" (print_ast_node (Literal(Number(1.)))))
+
+let%test "print unary operator" = String.equal "(- 5.000000)" (print_ast_node (Unary (Tokens.Minus,
+                                                                                      Literal(Number 5.))))
+let%test "print grouping" = String.equal "(- (Grouping false))" (print_ast_node (Unary (Tokens.Minus,
+                                                                                        Grouping (Literal(Bool false)))))
+let%test "print binary operator" = String.equal "(+ 1.000000 2.000000)" (print_ast_node (Binary
+                                                                                           ((Literal(Number 1.)),
+                                                                                            Tokens.Plus,
+                                                                                            (Literal(Number 2.)))))
