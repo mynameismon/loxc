24e8d9170d1af7136eab7d5465132dedf46b0215
mynameismon <42601474+mynameismon@users.noreply.github.com>
2024-07-13 01:11:57 +0530

    feat add internal errors

--- lib/error.ml
+++ lib/error.ml
@@ -2,9 +2,11 @@ type error =
   | LexError of string
   | SynError of string
   | RunTimeError of string
+  | InternalError of string
 
 let print_error error =
   match error with
   | (LexError str) -> Printf.sprintf "Lex Error: %s" str
   | (SynError str) -> Printf.sprintf "Syntax Error: %s" str
   | (RunTimeError str) -> Printf.sprintf "Runtime Error: %s" str
+  | (InternalError str) -> Printf.sprintf "INTERNAL ERROR!: %s" str
